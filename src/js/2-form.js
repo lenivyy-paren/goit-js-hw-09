dc8120faffb44b93942614ea8d38b0e7fda93381
Sverlink <vasiapypkin67@gmail.com>
2025-07-19 14:05:37 +0200

    hw-09

---
+++ src/js/2-form.js
@@ -0,0 +1,49 @@
+let formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+loadFormData();
+
+function loadFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+
+  if (!savedData) return;
+
+  try {
+    const parsedData = JSON.parse(savedData);
+    if (parsedData.email) {
+      form.elements.message.value = parsedData.message;
+      formData.message = parsedData.message;
+    }
+    if (parsedData.message) {
+      form.elements.message.value = parsedData.message;
+      formData.message = parsedData.message;
+    }
+  } catch (error) {
+    console.error('Error  localStorage', error);
+  }
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, message } = formData;
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+  formData = { email: '', message: '' };
+});
